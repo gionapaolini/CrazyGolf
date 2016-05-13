69ad1f6d9bfced3edff0313bc433d4cd9827c2f3
gionapaolini <gionapaolini@gmail.com>
2016-05-13 17:05:44 +0200

    Changes in MainGameLoop class to test shaders.

--- src/engineTester/MainGameLoop.java
+++ src/engineTester/MainGameLoop.java
@@ -5,6 +5,7 @@ import renderEngine.DisplayManager;
 import renderEngine.Loader;
 import renderEngine.RawModel;
 import renderEngine.Renderer;
+import shaders.StaticShader;
 
 /**
  * Created by giogio on 13/05/16.
@@ -16,23 +17,28 @@ public class MainGameLoop {
 
         Loader loader = new Loader();
         Renderer renderer = new Renderer();
+        StaticShader shader = new StaticShader();
 
-        float[] vertices = { -0.5f, 0.5f, 0f, -0.5f, -0.5f, 0f, 0.5f, -0.5f, 0f, 0.5f, -0.5f, 0f, 0.5f, 0.5f, 0f, -0.5f, 0.5f, 0f};
+        float[] vertices = { -0.5f, 0.5f, 0f,
+                             -0.5f, -0.5f, 0f,
+                             0.5f, -0.5f, 0f,
+                             0.5f, 0.5f, 0f};
+        int[] indices = {0,1,3,3,1,2};
 
-        RawModel model = loader.loadToVAO(vertices);
+        RawModel model = loader.loadToVAO(vertices, indices);
 
         while(!Display.isCloseRequested()){
 
             renderer.prepare();
-
+            shader.start();
             renderer.render(model);
-
+            shader.stop();
             DisplayManager.updateDisplay();
 
         }
 
         loader.cleanUp();
-
+        shader.cleanUp();
         DisplayManager.closeDisplay();
 
     }
