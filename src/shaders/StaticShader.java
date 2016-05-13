75c787858e3c5cd356032d341e82d0272c9aefc2
gionapaolini <gionapaolini@gmail.com>
2016-05-13 18:35:54 +0200

    Add method to bind uniform variables to the location in the java source

--- src/shaders/StaticShader.java
+++ src/shaders/StaticShader.java
@@ -1,5 +1,7 @@
 package shaders;
 
+import org.lwjgl.util.vector.Matrix4f;
+
 /**
  * Created by giogio on 13/05/16.
  */
@@ -7,6 +9,8 @@ public class StaticShader extends ShaderProgram{
 
     private static final String VERTEX_FILE = "src/shaders/vertexShader.txt";
     private static final String FRAGMENT_FILE = "src/shaders/fragmentShader.txt";
+
+    private int location_transformationMatrix;
 
     public StaticShader(){
         super(VERTEX_FILE,FRAGMENT_FILE);
@@ -16,5 +20,14 @@ public class StaticShader extends ShaderProgram{
     protected void bindAttributes() {
         super.bindAttributes(0,"position");
         super.bindAttributes(1,"textureCoords");
+    }
+
+    @Override
+    protected void getAllUniformLocations(){
+        location_transformationMatrix = super.getUniformLocation("transformationMatrix");
+    }
+
+    public void loadTransformationMatrix(Matrix4f matrix){
+        super.loadMatrix(location_transformationMatrix,matrix);
     }
 }
