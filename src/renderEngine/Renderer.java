6f44d13bcd783d95536ea9feaba07554966c02d4
gionapaolini <gionapaolini@gmail.com>
2016-05-13 14:10:49 +0200

    Small change for the Renderer class

--- src/renderEngine/Renderer.java
+++ src/renderEngine/Renderer.java
@@ -10,9 +10,8 @@ import org.lwjgl.opengl.GL30;
 public class Renderer {
 
     public void prepare() {
-        GL11.glClearColor(0, 1, 0, 1);
         GL11.glClear(GL11.GL_COLOR_BUFFER_BIT);
-
+        GL11.glClearColor(0, 1, 0, 1);
     }
 
     public void render(RawModel model){
