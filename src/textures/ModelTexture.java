46815b23895e84339d3530711dab4ad673a3c632
gionapaolini <gionapaolini@gmail.com>
2016-05-13 17:20:45 +0200

    New class ModelTexture, representing the single texture.

---
+++ src/textures/ModelTexture.java
@@ -0,0 +1,21 @@
+package textures;
+
+/**
+ * Created by giogio on 13/05/16.
+ */
+public class ModelTexture {
+
+    private int textureID;
+
+    public ModelTexture(int id){
+
+        this.textureID = id;
+
+    }
+
+    public int getTextureID(){
+        return textureID;
+    }
+
+
+}
