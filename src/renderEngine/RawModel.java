aea8f3792856eb842c19829a67e2fe8709886400
gionapaolini <gionapaolini@gmail.com>
2016-05-13 13:15:34 +0200

    Add RawModel class to the renderEngine package

---
+++ src/renderEngine/RawModel.java
@@ -0,0 +1,23 @@
+package renderEngine;
+
+/**
+ * Created by giogio on 13/05/16.
+ */
+public class RawModel {
+
+    private int vaoID;
+    private int vertexCount;
+
+    public RawModel(int vaoID, int vertexCount){
+        this.vaoID = vaoID;
+        this.vertexCount = vertexCount;
+    }
+
+    public int getVaoID() {
+        return vaoID;
+    }
+
+    public int getVertexCount() {
+        return vertexCount;
+    }
+}
