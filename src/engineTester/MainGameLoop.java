1d478115e05dd13b7dd99c9def5fccbae5e544db
gionapaolini <gionapaolini@gmail.com>
2016-05-13 13:55:48 +0200

    Changes on MainGameLoop to test the rendering.
    Use Loader, Renderer and RawModel classes.

--- src/engineTester/MainGameLoop.java
+++ src/engineTester/MainGameLoop.java
@@ -2,6 +2,9 @@ package engineTester;
 
 import org.lwjgl.opengl.Display;
 import renderEngine.DisplayManager;
+import renderEngine.Loader;
+import renderEngine.RawModel;
+import renderEngine.Renderer;
 
 /**
  * Created by giogio on 13/05/16.
@@ -11,11 +14,24 @@ public class MainGameLoop {
 
         DisplayManager.createDisplay();
 
+        Loader loader = new Loader();
+        Renderer renderer = new Renderer();
+
+        float[] vertices = { -0.5f, 0.5f, 0f, -0.5f, -0.5f, 0f, 0.5f, -0.5f, 0f, 0.5f, -0.5f, 0f, 0.5f, 0.5f, 0f, -0.5f, 0.5f, 0f};
+
+        RawModel model = loader.loadToVAO(vertices);
+
         while(!Display.isCloseRequested()){
+
+            renderer.prepare();
+
+            renderer.render(model);
 
             DisplayManager.updateDisplay();
 
         }
+
+        loader.cleanUp();
 
         DisplayManager.closeDisplay();
 
