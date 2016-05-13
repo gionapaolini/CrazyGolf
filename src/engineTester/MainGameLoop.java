ff75fa2781a894e083d780611b12bf42eafeb30d
gionapaolini <gionapaolini@gmail.com>
2016-05-13 09:32:05 +0200

    Create the MainGameLoop class, needed to run the GameDisplay

---
+++ src/engineTester/MainGameLoop.java
@@ -0,0 +1,23 @@
+package engineTester;
+
+import org.lwjgl.opengl.Display;
+import renderEngine.DisplayManager;
+
+/**
+ * Created by giogio on 13/05/16.
+ */
+public class MainGameLoop {
+    public static void main(String[] args){
+
+        DisplayManager.createDisplay();
+
+        while(!Display.isCloseRequested()){
+    
+            DisplayManager.updateDisplay();
+
+        }
+
+        DisplayManager.closeDisplay();
+
+    }
+}
