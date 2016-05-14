ac0e1bbb0ba548a5f074932e1ecf361353d8ed2c
gionapaolini <gionapaolini@gmail.com>
2016-05-14 14:03:58 +0200

    Add mousecontrol for the rotation of the camera

--- src/entities/Camera.java
+++ src/entities/Camera.java
@@ -1,6 +1,7 @@
 package entities;
 
 import org.lwjgl.input.Keyboard;
+import org.lwjgl.input.Mouse;
 import org.lwjgl.util.vector.Vector3f;
 
 
@@ -19,11 +20,25 @@ public class Camera {
         if(Keyboard.isKeyDown(Keyboard.KEY_W)){
             position.z -= 0.02f;
         }
+        if(Keyboard.isKeyDown(Keyboard.KEY_S)){
+            position.z += 0.02f;
+        }
         if(Keyboard.isKeyDown(Keyboard.KEY_D)){
             position.x += 0.02f;
         }
         if(Keyboard.isKeyDown(Keyboard.KEY_A)){
             position.x -= 0.02f;
+        }
+
+
+        if(Mouse.isButtonDown(1)){
+            float pitchChange = Mouse.getDY() * 0.1f;
+            pitch += pitchChange;
+        }
+
+        if (Mouse.isButtonDown(0)){
+            float angleChange = Mouse.getDX() * 0.1f;
+            yaw += angleChange;
         }
 
     }
