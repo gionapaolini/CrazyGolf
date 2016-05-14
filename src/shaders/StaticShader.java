7489421e2b7b239367ebb54d3f6b9100ff3d5024
gionapaolini <gionapaolini@gmail.com>
2016-05-15 00:29:53 +0200

    Add light

--- src/shaders/StaticShader.java
+++ src/shaders/StaticShader.java
@@ -1,6 +1,7 @@
 package shaders;
 
 import entities.Camera;
+import entities.Light;
 import org.lwjgl.util.vector.Matrix4f;
 import toolbox.Maths;
 
@@ -15,6 +16,8 @@ public class StaticShader extends ShaderProgram{
     private int location_transformationMatrix;
     private int location_projectionMatrix;
     private int location_viewMatrix;
+    private int location_lightPosition;
+    private int location_lightColour;
 
     public StaticShader(){
         super(VERTEX_FILE,FRAGMENT_FILE);
@@ -24,6 +27,7 @@ public class StaticShader extends ShaderProgram{
     protected void bindAttributes() {
         super.bindAttributes(0,"position");
         super.bindAttributes(1,"textureCoords");
+        super.bindAttributes(2,"normal");
     }
 
     @Override
@@ -31,6 +35,13 @@ public class StaticShader extends ShaderProgram{
         location_transformationMatrix = super.getUniformLocation("transformationMatrix");
         location_projectionMatrix = super.getUniformLocation("projectionMatrix");
         location_viewMatrix = super.getUniformLocation("viewMatrix");
+        location_lightPosition = super.getUniformLocation("lightPosition");
+        location_lightColour = super.getUniformLocation("lightColour");
+    }
+
+    public void loadLight(Light light){
+        super.loadVector(location_lightPosition, light.getPosition());
+        super.loadVector(location_lightColour, light.getColour());
     }
 
     public void loadViewMatrix(Camera camera){
