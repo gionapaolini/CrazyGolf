53b637241cf02734435902008ee0992363ac2726
gionapaolini <gionapaolini@gmail.com>
2016-05-14 18:15:47 +0200

    Add the module operations for yaw and pitch (0 - 360 degrees)

--- src/entities/Camera.java
+++ src/entities/Camera.java
@@ -1,5 +1,6 @@
 package entities;
 
+import org.lwjgl.Sys;
 import org.lwjgl.input.Keyboard;
 import org.lwjgl.input.Mouse;
 import org.lwjgl.util.vector.Vector3f;
@@ -34,12 +35,17 @@ public class Camera {
         if(Mouse.isButtonDown(1)){
             float pitchChange = Mouse.getDY() * 0.1f;
             pitch += pitchChange;
+            pitch%=360;
         }
 
         if (Mouse.isButtonDown(0)){
             float angleChange = Mouse.getDX() * 0.1f;
-            yaw += angleChange;
+            yaw -= angleChange;
+            yaw%=360;
         }
+
+
+
 
     }
 
