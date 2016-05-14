cb334a055af7b9c9b20ff0fedb3b7bdbbd98ad84
gionapaolini <gionapaolini@gmail.com>
2016-05-14 16:16:47 +0200

    Revert "Test that didn't work with camera motion"
    This reverts commit 28fcaffc130b6599dda5d8e3605cebf3b4d0cc68.

--- src/entities/Camera.java
+++ src/entities/Camera.java
@@ -17,32 +17,17 @@ public class Camera {
     public Camera(){}
 
     public void move(){
-
-        float nx = (float)(Math.sin(yaw));
-        float ny = (float)(Math.sin(pitch));
-        float nz = (float)(Math.cos(yaw));
-
-        float t = 0.2f;
-
         if(Keyboard.isKeyDown(Keyboard.KEY_W)){
-            position.x += nx*t;
-            position.y -= ny*t;
-            position.z -= nz*t;
+            position.z -= 0.02f;
         }
         if(Keyboard.isKeyDown(Keyboard.KEY_S)){
-            position.x -= nx*t;
-            position.y += ny*t;
-            position.z += nz*t;
+            position.z += 0.02f;
         }
         if(Keyboard.isKeyDown(Keyboard.KEY_D)){
-            yaw+=1f;
-            if(yaw>360)
-                yaw = 0;
+            position.x += 0.02f;
         }
         if(Keyboard.isKeyDown(Keyboard.KEY_A)){
-            yaw-=1f;
-            if(yaw<0)
-                yaw = 360;
+            position.x -= 0.02f;
         }
 
 
