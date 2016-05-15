8d1cd42596189308e7c392fda61146d0865b7ab0
gionapaolini <gionapaolini@gmail.com>
2016-05-15 08:50:18 +0200

    Add Rendering Optimizations

--- src/engineTester/MainGameLoop.java
+++ src/engineTester/MainGameLoop.java
@@ -6,13 +6,14 @@ import entities.Light;
 import models.TexturedModel;
 import org.lwjgl.opengl.Display;
 import org.lwjgl.util.vector.Vector3f;
-import renderEngine.DisplayManager;
-import renderEngine.Loader;
+import renderEngine.*;
 import models.RawModel;
-import renderEngine.OBJLoader;
-import renderEngine.Renderer;
 import shaders.StaticShader;
 import textures.ModelTexture;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 
 /**
  * Created by giogio on 13/05/16.
@@ -23,36 +24,45 @@ public class MainGameLoop {
         DisplayManager.createDisplay();
 
         Loader loader = new Loader();
-        StaticShader shader = new StaticShader();
-        Renderer renderer = new Renderer(shader);
 
         RawModel model = OBJLoader.loadObjModel("tree",loader);
-
         ModelTexture texture = new ModelTexture(loader.loadTexture("tree"));
         TexturedModel texturedModel = new TexturedModel(model, texture);
 
+        Random r = new Random();
+        List<Entity> trees = new ArrayList<Entity>();
+        for(int i=0;i<100;i++){
 
-        Entity entity = new Entity(texturedModel, new Vector3f(0,0,-25),0,0,0,1);
+            float x = (r.nextFloat()*50)-25;
+            float y = (r.nextFloat()*20)-10;
+            float z = (r.nextFloat()*50)-25;
+            float rx = r.nextFloat()*180;
+            float ry = r.nextFloat()*180;
+            float rz = r.nextFloat()*180;
+            float scale = r.nextFloat();
 
-        Light light = new Light(new Vector3f(0,0,-20), new Vector3f(1,1,1));
+            trees.add(new Entity(texturedModel, new Vector3f(x,y,z),rx,ry,rz,scale));
+
+        }
+
+        Light light = new Light(new Vector3f(0,1000,0), new Vector3f(1,1,1));
 
         Camera camera = new Camera();
+
+        MasterRenderer renderer = new MasterRenderer();
 
 
         while(!Display.isCloseRequested()){
             camera.move();
-            renderer.prepare();
-            shader.start();
-            shader.loadLight(light);
-            shader.loadViewMatrix(camera);
-            renderer.render(entity,shader);
-            shader.stop();
+            for(Entity entity:trees){
+                renderer.processEntity(entity);
+            }
+            renderer.render(light,camera);
             DisplayManager.updateDisplay();
 
         }
-
+        renderer.cleanUp();
         loader.cleanUp();
-        shader.cleanUp();
         DisplayManager.closeDisplay();
 
     }
--- src/renderEngine/Renderer.java
+++ src/renderEngine/Renderer.java
@@ -8,6 +8,9 @@ import org.lwjgl.util.vector.Matrix4f;
 import shaders.StaticShader;
 import toolbox.Maths;
 
+import java.util.List;
+import java.util.Map;
+
 /**
  * Created by giogio on 13/05/16.
  */
@@ -19,8 +22,12 @@ public class Renderer {
 
 
     private Matrix4f projectionMatrix;
+    private StaticShader shader;
 
     public Renderer(StaticShader shader){
+        this.shader = shader;
+        GL11.glEnable(GL11.GL_CULL_FACE);
+        GL11.glCullFace(GL11.GL_BACK);
         createProjectionMatrix();
         shader.start();
         shader.loadProjectionMatrix(projectionMatrix);
@@ -32,25 +39,45 @@ public class Renderer {
         GL11.glClearColor(0, 1, 0, 1);
     }
 
-    public void render(Entity entity, StaticShader shader){
-        TexturedModel texturedModel = entity.getModel();
-        RawModel rawModel = texturedModel.getRawModel();
+    public void render(Map<TexturedModel,List<Entity>> entities){
+        for(TexturedModel model: entities.keySet()){
+            prepareTexturedModel(model);
+            List<Entity> batch = entities.get(model);
+            for(Entity entity:batch){
+                prepareInstance(entity);
+                GL11.glDrawElements(GL11.GL_TRIANGLES,
+                        model.getRawModel().getVertexCount(),
+                        GL11.GL_UNSIGNED_INT,0);
+
+            }
+
+            unbindTexturedModel();
+        }
+    }
+
+    private void prepareTexturedModel(TexturedModel model){
+        RawModel rawModel = model.getRawModel();
         GL30.glBindVertexArray(rawModel.getVaoID());
         GL20.glEnableVertexAttribArray(0);
         GL20.glEnableVertexAttribArray(1);
         GL20.glEnableVertexAttribArray(2);
-        Matrix4f transformationMatrix = Maths.createTransformationMatrix(entity.getPosition(),
-                entity.getRotX(),entity.getRotY(),entity.getRotZ(),entity.getScale());
-        shader.loadTransformationMatrix(transformationMatrix);
         GL13.glActiveTexture(GL13.GL_TEXTURE0);
-        GL11.glBindTexture(GL11.GL_TEXTURE_2D, texturedModel.getTexture().getTextureID());
-        GL11.glDrawElements(GL11.GL_TRIANGLES,rawModel.getVertexCount(), GL11.GL_UNSIGNED_INT,0);
+        GL11.glBindTexture(GL11.GL_TEXTURE_2D, model.getTexture().getTextureID());
+
+    }
+
+    private void unbindTexturedModel(){
         GL20.glDisableVertexAttribArray(0);
         GL20.glDisableVertexAttribArray(1);
         GL20.glDisableVertexAttribArray(2);
         GL30.glBindVertexArray(0);
     }
 
+    private void prepareInstance(Entity entity){
+        Matrix4f transformationMatrix = Maths.createTransformationMatrix(entity.getPosition(),
+                entity.getRotX(),entity.getRotY(),entity.getRotZ(),entity.getScale());
+        shader.loadTransformationMatrix(transformationMatrix);
+    }
 
     private void createProjectionMatrix(){
         float aspectRatio = (float) Display.getWidth() / (float) Display.getHeight();
