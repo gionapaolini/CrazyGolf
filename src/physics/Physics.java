9dde7d0ba6a7d481044d78420c8b4033412a22e2
jlvelasquezsosa <jvelasquezsosa96@gmail.com>
2016-05-26 21:38:15 +0200

    bounce of floor and moves

--- src/physics/Physics.java
+++ src/physics/Physics.java
@@ -9,7 +9,7 @@ public class Physics {
 	
 
 	private static final Vector3D WORLD_UP = new Vector3D(0, 1 , 0);
-	private static final float GRAVITY = -0.0098f;
+	private static float GRAVITY = -0.0098f;
 	public static final float ROLL_FRIC = 0.9670f;
 	private static float minVelocity = 0.001f;
 	
@@ -19,16 +19,30 @@ public class Physics {
 		calcSlope(b, normal);
 		
 		if(b.isMoving){
-			b.vel = b.vel.mulitply(ROLL_FRIC);
+			//b.vel = b.vel.mulitply(ROLL_FRIC);
+			b.vel = new Vector3D(b.vel.x * ROLL_FRIC, b.vel.y + GRAVITY, b.vel.z *ROLL_FRIC);
+			
+			System.out.println(" y: " + b.vel.y);
+			System.out.println(" x: " + b.vel.x);
+			System.out.println(" z: " + b.vel.z);
 			b.pos = b.pos.add(b.vel.multiply(b.dir));
 			b.move();
+			//
 			
-			if((b.vel.absolute().x<=minVelocity 
-			   && b.vel.absolute().y<=minVelocity 
-			   && b.vel.absolute().z<=minVelocity)
-			  //|| checkBallInHole(b,p)
+			if(b.vel.absolute().x<=minVelocity 
+			   && b.vel.absolute().y<=0.0099
+			   && b.vel.absolute().z<=minVelocity
+			   && b.pos.y == 0.0f
 			   ){
+			   
 					b.isMoving = false;
+			}
+			if (b.pos.y <= 0){
+				reflect(b,WORLD_UP);
+				if(Math.abs(b.vel.y)>= 0.06f )
+					GRAVITY = -GRAVITY;
+				else
+					b.vel.y = 0;
 			}
 			//TODO: Handle Collision all together
 		}
